5216a979391d5257f8789441a021c46c90c5f757
Douglas Volcato <douglasvolcato@gmail.com>
2022-12-16 18:48:46 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct Body

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,10 +1,13 @@
 import { HttpPostClientSpy } from "../../test/mock-http-client";
+import { mockAuthentication } from "../../test/test/mock-authentication";
 import { RemoteAuthentication } from "./remote-authentication";
 
 type SutTypes = {
   sut: RemoteAuthentication;
   httpPostClientSpy: HttpPostClientSpy;
 };
+
+const url = "other_url";
 
 const makeSut = (url = "any_url"): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy();
@@ -14,9 +17,14 @@ const makeSut = (url = "any_url"): SutTypes => {
 
 describe("RemoteAuthentication", () => {
   test("Should call HttpPostClient with correct URL.", async () => {
-    const url = "other_url";
     const { sut, httpPostClientSpy } = makeSut(url);
-    await sut.auth();
+    await sut.auth(mockAuthentication());
     expect(httpPostClientSpy.url).toBe(url);
+  });
+
+  test("Should call HttpPostClient with correct Body.", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    await sut.auth(mockAuthentication());
+    expect(httpPostClientSpy.body).toEqual(mockAuthentication());
   });
 });
