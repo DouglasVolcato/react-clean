8765e818309587302752f29ffea5b32f6d06d9a3
Douglas Volcato <douglasvolcato@gmail.com>
2022-12-17 12:05:40 -0300

    test: ensure AxiosHttpClient returns the correct statusSoce and body

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -2,8 +2,14 @@ import { AxiosHttpClient } from "./axios-http-client";
 import axios from "axios";
 import { HttpPostParams } from "../../../data/protocols/http/http-post-client";
 
+const mockedAxiosResult = {
+  data: { email: "test_email" },
+  status: 200,
+};
+
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+mockedAxios.post.mockResolvedValue(mockedAxiosResult);
 
 const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient();
@@ -22,5 +28,15 @@ describe("AxiosHttpClient", () => {
     const sut = makeSut();
     await sut.post(request);
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
+  });
+
+  test("Should return the correct statusSoce and body.", async () => {
+    const request = mockPostRequest();
+    const sut = makeSut();
+    const httpResponse = await sut.post(request);
+    expect(httpResponse).toEqual({
+      statusCode: mockedAxiosResult.status,
+      body: mockedAxiosResult.data,
+    });
   });
 });
