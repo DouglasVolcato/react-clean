ffae3773f5d7990be57f12142570809fcd91eacd
Douglas Volcato <douglasvolcato@gmail.com>
2022-12-18 16:57:25 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,29 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, RenderResult } from "@testing-library/react";
 import Login from "./login";
+
+type SutTypes = {
+  sut: RenderResult;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />);
+  return { sut };
+};
 
 describe("Login Page", () => {
   test("Should not render spinner nor error on start.", () => {
-    const { getByTestId } = render(<Login />);
-    const errorWrap = getByTestId("errorWrap");
+    const { sut } = makeSut();
+    const errorWrap = sut.getByTestId("errorWrap");
     expect(errorWrap.childElementCount).toBe(0);
   });
 
   test("Should start with initial button disabled.", () => {
-    const { getByTestId } = render(<Login />);
-    const submitButton = getByTestId("submit") as HTMLButtonElement;
+    const { sut } = makeSut();
+    const submitButton = sut.getByTestId("submit") as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
   });
 
   test("Should start with email input title as required field.", () => {
-    const { getByTestId } = render(<Login />);
-    const emailInput = getByTestId("email-status");
+    const { sut } = makeSut();
+    const emailInput = sut.getByTestId("email-status");
     expect(emailInput.title).toBe("Required field");
   });
 
-  test("Should start with password input title as required.", () => {
-    const { getByTestId } = render(<Login />);
-    const passwordInput = getByTestId("password-status");
+  test("Should start with password input title as required field.", () => {
+    const { sut } = makeSut();
+    const passwordInput = sut.getByTestId("password-status");
     expect(passwordInput.title).toBe("Required field");
   });
 });
