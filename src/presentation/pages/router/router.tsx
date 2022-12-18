2b7134babaaa04ff3ced1938b3ce7cbc6a3436f3
Douglas Volcato <douglasvolcato@gmail.com>
2022-12-18 14:11:00 -0300

    feat: add app router

---
+++ src/presentation/pages/router/router.tsx
@@ -0,0 +1,16 @@
+import React, { memo } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../login/login";
+import "../../styles/global.scss";
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default memo(App);
