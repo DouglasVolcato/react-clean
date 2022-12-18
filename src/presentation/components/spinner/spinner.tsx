5fb41e374367e39c895c048d9f226b7672b05268
Douglas Volcato <douglasvolcato@gmail.com>
2022-12-18 11:51:40 -0300

    refactor: implement spnner close divs

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -4,10 +4,10 @@ import "./spinner-styles.scss";
 const Spinner: React.FC = () => {
   return (
     <div className="spinner">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div />
+      <div />
+      <div />
+      <div />
     </div>
   );
 };
