94c2bbce637a5c8375f04e3004fb9614e1e336dc
Douglas Volcato <douglasvolcato@gmail.com>
2022-12-18 17:32:22 -0300

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -60,12 +60,21 @@ describe("Login Page", () => {
     expect(passwordStatusInput.title).toBe("Required field");
   });
 
-  test("Should call validation with correct values.", () => {
+  test("Should call validation with correct email.", () => {
     const { sut, validationSpy } = makeSut();
     const emailInput = sut.getByTestId("email") as HTMLInputElement;
     fireEvent.input(emailInput, { target: { value: "any_email" } });
     expect(validationSpy.input).toEqual({
       email: "any_email",
+    });
+  });
+
+  test("Should call validation with correct password.", () => {
+    const { sut, validationSpy } = makeSut();
+    const passwordInput = sut.getByTestId("password") as HTMLInputElement;
+    fireEvent.input(passwordInput, { target: { value: "any_password" } });
+    expect(validationSpy.input).toEqual({
+      password: "any_password",
     });
   });
 });
