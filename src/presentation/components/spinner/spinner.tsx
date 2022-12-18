fea504e59af23c6ab8e5827df68017b378719972
Douglas Volcato <douglasvolcato@gmail.com>
2022-12-17 23:53:45 -0300

    feat: add login view

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./spinner-styles.scss";
+
+const Spinner: React.FC = () => {
+  return (
+    <div className="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Spinner;
---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./login-styles.scss";
+import Spinner from "../../components/spinner/spinner";
+
+const Login: React.FC = () => {
+  return (
+    <div className="login">
+      <header className="header">
+        <img className="image" src="./src\presentation\pages\login\logo.png" />
+        <h1>React Webpage</h1>
+      </header>
+      <form className="form">
+        <h2>Login</h2>
+        <div className="inputWrap">
+          <input type="email" name="email" placeholder="Write your email" />
+          <span className="status"></span>
+        </div>
+        <div className="inputWrap">
+          <input
+            type="password"
+            name="password"
+            placeholder="Write your password"
+          />
+          <span className="status"></span>
+        </div>
+        <button className="submit" type="submit">
+          Send
+        </button>
+        <span className="link">Create an account</span>
+        <div className="errorWrap">
+          <Spinner />
+          <span className="error">Error</span>
+        </div>
+      </form>
+      <footer className="footer">Footer</footer>
+    </div>
+  );
+};
+
+export default Login;
